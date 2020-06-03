4d516a1538e50d53a8d0f000235354745e13e647
Ramanpreet6262 <ramanpreet6262@gmail.com>
2020-06-03 12:02:27 +0530

    made navbar sticky and added favicon

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -5,43 +5,41 @@ import './Navbar.css';
 
 const navbar = props => {
   return (
-    <div className='navigation'>
-      <Navbar bg='dark' variant='dark' expand='lg'>
-        <Navbar.Brand as={Link} to='/'>
-          Typing Test
-        </Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse>
-          <Nav className='ml-auto'>
-            {props.isAuthenticated ? (
-              <>
-                <NavItem>
-                  <Nav.Link as={Link} to='/profile'>
-                    Profile
-                  </Nav.Link>
-                </NavItem>
-                <NavItem className='logout-link' onClick={props.handleLogout}>
-                  Logout
-                </NavItem>
-              </>
-            ) : (
-              <>
-                <NavItem>
-                  <Nav.Link as={Link} to='/signup'>
-                    Signup
-                  </Nav.Link>
-                </NavItem>
-                <NavItem>
-                  <Nav.Link as={Link} to='/login'>
-                    Login
-                  </Nav.Link>
-                </NavItem>
-              </>
-            )}
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </div>
+    <Navbar bg='dark' variant='dark' expand='lg' sticky='top'>
+      <Navbar.Brand as={Link} to='/'>
+        Typing Test
+      </Navbar.Brand>
+      <Navbar.Toggle />
+      <Navbar.Collapse>
+        <Nav className='ml-auto'>
+          {props.isAuthenticated ? (
+            <>
+              <NavItem>
+                <Nav.Link as={Link} to='/profile'>
+                  Profile
+                </Nav.Link>
+              </NavItem>
+              <NavItem className='logout-link' onClick={props.handleLogout}>
+                Logout
+              </NavItem>
+            </>
+          ) : (
+            <>
+              <NavItem>
+                <Nav.Link as={Link} to='/signup'>
+                  Signup
+                </Nav.Link>
+              </NavItem>
+              <NavItem>
+                <Nav.Link as={Link} to='/login'>
+                  Login
+                </Nav.Link>
+              </NavItem>
+            </>
+          )}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
