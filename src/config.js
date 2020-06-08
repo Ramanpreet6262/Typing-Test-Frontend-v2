bf70a4716a24cd7b777d6b71f48b3b12bde9501c
Ramanpreet6262 <ramanpreet6262@gmail.com>
2020-06-09 00:37:38 +0530

    added .env for api keys

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
-import Keys from './SecretKeys/keys';
+// import Keys from './SecretKeys/keys';
 
 export default {
   cognito: {
     REGION: 'us-east-2',
-    USER_POOL_ID: Keys.userPoolId,
-    APP_CLIENT_ID: Keys.appClientId,
-    IDENTITY_POOL_ID: Keys.identityPoolId
+    USER_POOL_ID: process.env.REACT_APP_USER_POOL_ID,
+    APP_CLIENT_ID: process.env.REACT_APP_APP_CLIENT_ID,
+    IDENTITY_POOL_ID: process.env.REACT_APP_IDENTITY_POOL_ID
   }
 };
